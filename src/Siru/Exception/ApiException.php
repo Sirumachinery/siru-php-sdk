7ad87242a78c192a6f49057c4837bc384ab6ca89
Joni Halme <joni.halme@sirumobile.com>
2020-03-26 09:12:19 +0200

    dropped AbstractApiException, add SiruExceptionInterface

--- src/Siru/Exception/ApiException.php
+++ src/Siru/Exception/ApiException.php
@@ -6,10 +6,19 @@ namespace Siru\Exception;
  * In case of Payment API, there can be multiple error messages returned at once. You can retrieve list
  * of these using getErrorStack().
  */
-class ApiException extends AbstractApiException
+class ApiException extends \RuntimeException implements SiruExceptionInterface
 {
     
     private $errorStack = [];
+
+    private $responseBody;
+
+    public function __construct($message = '', $code = 0, \Exception $e = null, ?string $body = '')
+    {
+        parent::__construct($message, $code, $e);
+
+        $this->responseBody = $body;
+    }
 
     public static function create(?int $httpStatus, string $body) : ApiException
     {
@@ -33,6 +42,16 @@ class ApiException extends AbstractApiException
         $exception = new self($message, $httpStatus ?: 0, null, $body);
         $exception->errorStack = $errorStack;
         return $exception;
+    }
+
+    /**
+     * Get raw message body from HTTP response if available.
+     *
+     * @return string|null
+     */
+    public function getResponseBody() : ?string
+    {
+        return $this->responseBody;
     }
 
     /**
--- src/Siru/Exception/TransportException.php
+++ src/Siru/Exception/TransportException.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Siru\Exception;
 
-class TransportException extends \RuntimeException
+class TransportException extends \RuntimeException implements SiruExceptionInterface
 {
 
 }
